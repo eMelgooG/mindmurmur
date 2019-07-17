268e3a3f17125687f96a353c26b49bc57700b049
Chris Adams <chris@caams.com.au>
2019-07-17 02:09:39 -0700

    Use WAV for audio

--- web/scripts/run_sound.ps1
+++ web/scripts/run_sound.ps1
@@ -1,8 +1,8 @@
-$params = "--audio_folder ../../fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/mp3/audio --up_transition_sound_filename ../../fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/mp3/transition_up.mp3  --down_transition_sound_filename ../../fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/mp3/transition_down.mp3"
-py -2 "c:/Users/aurel/OneDrive/Documents/DEV ROOT/PYTHON/mindmurmer/fr0st-master/fr0st/scripts/mindmurmer/sound_controller.py" `
---audio_folder "C:\Users\aurel\OneDrive\Documents\DEV ROOT\PYTHON\mindmurmer\fr0st-master\fr0st\scripts\mindmurmer\sounds_controller_files\16000hz-24bit\mp3\audio"`
---up_transition_sound_filename "C:\Users\aurel\OneDrive\Documents\DEV ROOT\PYTHON\mindmurmer\fr0st-master\fr0st\scripts\mindmurmer\sounds_controller_files/16000hz-24bit/mp3/transition_up.mp3"`
---down_transition_sound_filename "C:\Users\aurel\OneDrive\Documents\DEV ROOT\PYTHON\mindmurmer\fr0st-master\fr0st\scripts\mindmurmer\sounds_controller_files/16000hz-24bit/mp3/transition_down.mp3"
+$params = "--audio_folder ../../fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/wav/audio --up_transition_sound_filename ../../fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/wav/transition_up.wav  --down_transition_sound_filename ../../fr0st-master/fr0st/scripts/mindmurmer/sounds_controller_files/16000hz-24bit/wav/transition_down.wav"
+py -2 "c:/Users/Mind Murmur/Documents/mindmurmer/fr0st-master/fr0st/scripts/mindmurmer/sound_controller.py" `
+--audio_folder "C:\Users\Mind Murmur\Documents\mindmurmer\fr0st-master\fr0st\scripts\mindmurmer\sounds_controller_files\16000hz-24bit\wav\audio"`
+--up_transition_sound_filename "C:\Users\Mind Murmur\Documents\mindmurmer\fr0st-master\fr0st\scripts\mindmurmer\sounds_controller_files/16000hz-24bit/wav/transition_up.wav"`
+--down_transition_sound_filename "C:\Users\Mind Murmur\Documents\mindmurmer\fr0st-master\fr0st\scripts\mindmurmer\sounds_controller_files/16000hz-24bit/wav/transition_down.wav"
 
 
  
