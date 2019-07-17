302ff08a86231f830a0bc6732f56940f58e54d0c
Chris Adams <chris@caams.com.au>
2019-07-17 02:34:15 -0700

    Clean up top level

--- minfmurmur_dependecies.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pip install numpy
-sudo apt-get install build-essential libtool autoconf libpng12-dev libjpeg62-dev libxml2-dev python-dev python-numpy python-wxgtk2.8 subversion python-pyaudio python2.7-pyside python-wxgtk3.0
