cb247979bcef7c80f04413784662d5875887a89f
Chris Adams <chris@caams.com.au>
2019-08-14 19:04:49 -0700

    Add start script for heart sensor

--- run_all.bat
+++ run_all.bat
@@ -2,4 +2,5 @@ start run_visuals.bat
 start run_lights.bat
 start run_osc.bat
 start run_sound.bat
+start run_heart_sensor.bat
 start run_web.bat
---
+++ run_heart_sensor.bat
@@ -0,0 +1,2 @@
+cmd /c ".\env3\scripts\activate && python heart_sensor_service\heartrate_sensor_service.py"
+pause
